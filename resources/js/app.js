ae79e0a140be2181587d80344c9f84a7b857e243
Maryame <faitah.maryame@gmail.com>
2020-04-16 12:36:20 +0200

    first commit

---
+++ resources/js/app.js
@@ -0,0 +1,13 @@
+require('jquery');
+global.$ = global.jQuery = require('jquery');
+require('./bootstrap');
+require('superfish/dist/js/hoverIntent');
+require('superfish/dist/js/superfish');
+require('morphext/dist/morphext');
+require('wow.js');
+require('./sticky');
+global.sticky = require('./sticky');
+require('jquery.easing');
+
+require('./custom');
+require('./contactform')
