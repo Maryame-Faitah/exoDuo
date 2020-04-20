2609b317fc4ffca1c6e5d723114b59038a9b203e
ade-re10 <andreandre_1234@hotmail.com>
2020-04-20 15:27:59 +0200

    123

--- resources/views/admin/testimonials/create.blade.php
+++ resources/views/admin/testimonials/create.blade.php
@@ -3,7 +3,6 @@
     <h1 class="text-center">Créer</h1>
     <div class="col mt-5">
         <div class="card card-warning">
-<<<<<<< HEAD
           <div class="card-body">
             <form action="{{route('testimonials1.store')}}" method="POST" enctype="multipart/form-data">
                 @csrf
@@ -20,25 +19,7 @@
                 <button type="submit" class="btn btn-primary">Ajouter</button>
             </form>
           </div>
-=======
-            <div class="card-body">
-                <form action="{{route('testimonials1.store')}}" method="POST" enctype="multipart/form-data">
-                    @csrf
-                    <div class="form-group">
-                      <label for="">Titre de la Section</label>
-                      <input type="text" name="section_title" id="section_title" class="form-control" placeholder="" aria-describedby="helpId">
-                    </div>
-            
-                    <div class="form-group">
-                        <label for="">Description de la Section</label>
-                        <input type="text" name="section_description" id="section_description" class="form-control" placeholder="" aria-describedby="helpId">
-                    </div>
-            
-                    <button type="submit" class="btn btn-primary">Ajouter</button>
-                </form>
 
-            </div>
->>>>>>> c5fecfb0dcc7db24dc448405467f8b863354f006
         </div>
     </div>
 @endsection
--- resources/views/admin/testimonials/edit.blade.php
+++ resources/views/admin/testimonials/edit.blade.php
@@ -3,7 +3,6 @@
     <h1 class="text-center">Modifier</h1>
     <div class="col mt-5">
         <div class="card card-warning">
-<<<<<<< HEAD
           <div class="card-body">
             <form action="{{route('testimonials1.update', $testimonial->id)}}" method="POST" enctype="multipart/form-data">
                 @csrf
@@ -21,25 +20,6 @@
                 <button type="submit" class="btn btn-primary">Ajouter</button>
             </form>
           </div>
-=======
-            <div class="card-body">
-                <form action="{{route('testimonials1.update', $testimonial->id)}}" method="POST" enctype="multipart/form-data">
-                    @csrf
-                    @method('put')
-                    <div class="form-group">
-                      <label for="">Titre de la Section</label>
-                      <input type="text" name="section_title" value="{{$testimonial->section_title}}" id="section_title" class="form-control" placeholder="" aria-describedby="helpId">
-                    </div>
-            
-                    <div class="form-group">
-                        <label for="">Description de la Section</label>
-                        <input type="text" name="section_description" value="{{$testimonial->section_description}}" id="section_description" class="form-control" placeholder="" aria-describedby="helpId">
-                    </div>
-            
-                    <button type="submit" class="btn btn-primary">Ajouter</button>
-                </form>
-            </div>
->>>>>>> c5fecfb0dcc7db24dc448405467f8b863354f006
         </div>
     </div>
 @endsection
